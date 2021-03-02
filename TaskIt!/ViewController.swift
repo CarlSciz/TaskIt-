97f1695a1e5345b8aa79b553e123c5616a2af88b
Carl Scozzari <Cscozzari17@gmail.com>
2021-03-02 09:28:01 -0500

    Initial Commit

---
+++ TaskIt!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskIt!
+//
+//  Created by Carl Scozzari on 3/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
